783e6dc5e7740297f865b138e2a602ed851b52fc
Brian <bdev@btro.jp>
2025-01-05 17:48:45 +0900

    Init

---
+++ app/(tabs)/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#2a2a3c",
+  },
+  whiteText: {
+    color: "#fff",
+  },
+});
+
+export default styles;
