c0f533d9920e76029b2ed80eb31c8e56c7f64c03
Brian <bdev@btro.jp>
2025-01-19 13:47:55 +0900

    Added custom color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
+    colors: {
+      btc100: "#D4F1F4",
+      btc200: "#75E6DA",
+      btc300: "#189AB4",
+      btc400: "#05445E",
+    },
     extend: {},
   },
   plugins: [],
