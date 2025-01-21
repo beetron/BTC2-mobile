12d094f96eb0064c5f12cfa58b1c14b2633235dc
Brian <bdev@btro.jp>
2025-01-21 17:08:43 +0900

    Removed unused code

--- app/(tabs)/logout.tsx
+++ app/(tabs)/logout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useRouter } from "expo-router";
 
