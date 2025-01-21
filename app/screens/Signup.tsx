40f5fc7ee645f88725424df6ca12d66423cac1e0
Brian <bdev@btro.jp>
2025-01-21 21:45:13 +0900

    Init. Signup screen

---
+++ app/screens/Signup.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Signup = () => {
+  return (
+    <View className="flex-1 w-full bg-btc400 items-center justify-center">
+      <Text className="font-funnel-regular text-2xl color-btc100">Signup</Text>
+    </View>
+  );
+};
+
+export default Signup;
