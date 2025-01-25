e94600b58114ec3ed8c9ef2ee688abd05a04b2ca
Brian <bdev@btro.jp>
2025-01-25 20:21:39 +0900

    WIP Header for (tabs)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,6 +3,9 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { Redirect, Tabs } from "expo-router";
 import { useAuth } from "../context/AuthContext";
+import React from "react";
+
+import Header from "@/components/Header";
 
 const TabLayout = () => {
   const { authState } = useAuth();
@@ -12,52 +15,55 @@ const TabLayout = () => {
   }
 
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: "skyblue",
-        tabBarStyle: {
-          backgroundColor: "#2a2a3c",
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome5 size={24} name="user-friends" color={color} />
-          ),
+    <>
+      <Header />
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: "skyblue",
+          tabBarStyle: {
+            backgroundColor: "#2a2a3c",
+          },
         }}
-      />
-      <Tabs.Screen
-        name="editFriends"
-        options={{
-          title: "Add/Remove",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome5 size={24} name="user-edit" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: "Settings",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="settings-sharp" size={24} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="logout"
-        options={{
-          title: "Logout",
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="logout" size={24} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome5 size={24} name="user-friends" color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="editFriends"
+          options={{
+            title: "Add/Remove",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome5 size={24} name="user-edit" color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="settings"
+          options={{
+            title: "Settings",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="settings-sharp" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="logout"
+          options={{
+            title: "Logout",
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="logout" size={24} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   );
 };
 
---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Header = () => {
+  return (
+    <View className="flex-1 h-20">
+      <Text>Header</Text>
+    </View>
+  );
+};
+
+export default Header;
