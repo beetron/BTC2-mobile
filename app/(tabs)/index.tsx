019288287ec1241385812cb6195097541fd235b5
Brian <bdev@btro.jp>
2025-01-27 19:51:31 +0900

    wip

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,20 @@
-import React from "react";
-import { Text, View } from "react-native";
-import styles from "../styles";
+import { useEffect } from "react";
+import FriendContainer from "@/components/FriendContainer";
+import { View } from "react-native";
+import useGetMyFriends from "@/hooks/useGetMyFriends";
 
+// Home screeen with friend list
 const Index = () => {
+  // Get user's friend list from API
+  const { myFriends } = useGetMyFriends();
+
+  // useEffect(() => {
+  //   getMyFriends();
+  // }, [getMyFriends]);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.whiteText}>Hello, world!</Text>
+    <View className="flex-1 justify-center items-center bg-btc500">
+      <FriendContainer />
     </View>
   );
 };
---
+++ components/FriendContainer.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const FriendContainer = (friendList) => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-xl text-btc100">FriendContainer</Text>
+    </View>
+  );
+};
+
+export default FriendContainer;
