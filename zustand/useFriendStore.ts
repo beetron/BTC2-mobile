446b413949b96ab0dfe3b0674246d38302ca42c4
Brian <bdev@btro.jp>
2025-02-11 22:03:05 +0900

    Installed Zustand and store data

---
+++ zustand/useFriendStore.ts
@@ -0,0 +1,29 @@
+import { create } from "zustand";
+
+interface Friend {
+  _id: string;
+  nickname: string;
+  profilePhoto: string;
+  updatedAt: string;
+}
+
+interface FriendStore {
+  selectedFriend: Friend | null;
+  setSelectedFriend: (friend: Friend) => void;
+  messages: string[];
+  setMessages: (messages: string[]) => void;
+  shouldRender: boolean; // used for useEffect to reload via socket event
+  setShouldRender: () => void;
+}
+
+const useFriendStore = create<FriendStore>((set) => ({
+  selectedFriend: null,
+  setSelectedFriend: (selectedFriend) => set({ selectedFriend }),
+  messages: [],
+  setMessages: (messages) => set({ messages }),
+  shouldRender: false,
+  setShouldRender: () =>
+    set((state) => ({ shouldRender: !state.shouldRender })),
+}));
+
+export default useFriendStore;
