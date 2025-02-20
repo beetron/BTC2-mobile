6b95aaf09410fcdbbcb245677698a334968126d4
Brian <bdev@btro.jp>
2025-02-20 20:56:17 +0900

    Update Friend.tsx

--- components/Friend.tsx
+++ components/Friend.tsx
@@ -1,6 +1,8 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
 import React from "react";
-import { Image } from "react-native";
+import { StyleSheet, Image } from "react-native";
+import { useRouter } from "expo-router";
+import useFriendStore from "../zustand/useFriendStore";
 
 const placeholderImage = require("../assets/images/placeholder_profile_pic.png");
 
@@ -8,33 +10,51 @@ const Friend = ({
   friend,
 }: {
   friend: {
+    _id: string;
     nickname: string;
     profilePhoto: string;
     unreadMessages: boolean;
   };
 }) => {
   const { nickname, profilePhoto, unreadMessages } = friend;
+  const { setSelectedFriend } = useFriendStore();
+  const router = useRouter();
+
+  const handleOnPress = () => {
+    setSelectedFriend(friend);
+    router.push("../app/member-screens/conversation");
+  };
+
   return (
-    <View className="flex-row items-center p-2 m-1 border-0 border-btc200">
-      <View className="flex-row justify-start">
-        <Image source={placeholderImage} className="w-16 h-16" />
-      </View>
-      <View className="flex-row justify-start ml-4">
-        <Text className="text-btc100 text-xl font-funnel-semi-bold">
-          {nickname}
-        </Text>
-      </View>
-      <View className="flex-1 justify-end flex-row">
-        {unreadMessages ? (
-          <Text className="text-btc200 font-funnel-medium">
-            Unread Message(s)
+    <TouchableOpacity onPress={handleOnPress}>
+      <View className="flex-row items-center p-2 m-1 border-0 border-btc200">
+        <View className="flex-row justify-start">
+          <Image source={placeholderImage} className="w-16 h-16" />
+        </View>
+        <View className="flex-row justify-start ml-4">
+          <Text className="text-btc100 text-xl font-funnel-semi-bold">
+            {nickname}
           </Text>
-        ) : (
-          <Text className="text-btc200 font-funnel-medium"></Text>
-        )}
+        </View>
+        <View className="flex-1 justify-end flex-row">
+          {unreadMessages ? (
+            <Text className="text-btc200 font-funnel-medium">
+              Unread Message(s)
+            </Text>
+          ) : (
+            <Text className="text-btc200 font-funnel-medium"></Text>
+          )}
+        </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  friend: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
 
 export default Friend;
