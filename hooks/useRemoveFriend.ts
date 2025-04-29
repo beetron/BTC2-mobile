568822bdb8f1f2abe2a14c0c55666d2d15432939
Brian <bdev@btro.jp>
2025-04-29 19:32:34 +0900

    Add useRemoveFriend hook

---
+++ hooks/useRemoveFriend.ts
@@ -0,0 +1,36 @@
+import { Alert } from "react-native";
+import axiosClient from "../utils/axiosClient";
+import { useState } from "react";
+
+const useRemoveFriend = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const removeFriend = async (uniqueId: string): Promise<boolean> => {
+    try {
+      setIsLoading(true);
+
+      const response = await axiosClient.put(`/users/removefriend/${uniqueId}`);
+
+      if (response.status === 200) {
+        Alert.alert("Friend Removed");
+        return true;
+      }
+      return false;
+    } catch (error: any) {
+      if (error.response && error.response.data && error.response.data.error) {
+        Alert.alert("Error", error.response.data.error);
+        console.error("Error in useRemoveFriend: ", error);
+      } else {
+        Alert.alert("Error", "An unexpected error occurred.");
+        console.error("Unexpected error in useRemoveFriend: ", error);
+      }
+      return false;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { removeFriend, isLoading };
+};
+
+export default useRemoveFriend;
