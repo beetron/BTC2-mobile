e872d69acf8a655fc6efd6dee2bbb03234ea1487
Brian <bdev@btro.jp>
2025-04-29 19:45:50 +0900

    Removed authState check.  axiosClient handles token check and re-routing of user.

--- hooks/useGetMyFriends.ts
+++ hooks/useGetMyFriends.ts
@@ -1,6 +1,5 @@
 import { useEffect, useState, useCallback } from "react";
 import axiosClient from "../utils/axiosClient";
-import { useAuth } from "../context/AuthContext";
 import { useRouter } from "expo-router";
 import useGetProfileImage from "./useGetProfileImage";
 
@@ -15,50 +14,48 @@ interface Friend {
 }
 
 const useGetMyFriends = () => {
-  const { authState } = useAuth();
   const [myFriends, setMyFriends] = useState<Friend[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const router = useRouter();
   const { getProfileImage } = useGetProfileImage();
 
   const getMyFriends = useCallback(async () => {
     try {
-      if (authState?.authenticated === true) {
-        const res = await axiosClient.get("/users/friendlist");
-        console.log("API response data:", res.data);
+      setIsLoading(true);
+      const res = await axiosClient.get("/users/friendlist");
+      console.log("API response data:", res.data);
 
-        // Call getProfileImage hook for each friend's profile image
-        const friendsWithImages = await Promise.all(
-          res.data.map(async (friend: Friend, index: number) => {
-            // console.log(`Processing friend ${index}:`, friend);
-            // console.log("friend.profileImage: ", friend.profileImage);
+      // Call getProfileImage hook for each friend's profile image
+      const friendsWithImages = await Promise.all(
+        res.data.map(async (friend: Friend, index: number) => {
+          // console.log(`Processing friend ${index}:`, friend);
+          // console.log("friend.profileImage: ", friend.profileImage);
 
-            let imageData = null;
-            if (friend.profileImage) {
-              try {
-                imageData = await getProfileImage(friend.profileImage);
-              } catch (imageError) {
-                console.error(
-                  `Error loading image for friend ${index}:`,
-                  imageError
-                );
-              }
+          let imageData = null;
+          if (friend.profileImage) {
+            try {
+              imageData = await getProfileImage(friend.profileImage);
+            } catch (imageError) {
+              console.error(
+                `Error loading image for friend ${index}:`,
+                imageError
+              );
             }
-            return {
-              ...friend,
-              profileImageData: imageData,
-            };
-          })
-        );
-        setMyFriends(friendsWithImages);
-      }
+          }
+          setIsLoading(false);
+          return {
+            ...friend,
+            profileImageData: imageData,
+          };
+        })
+      );
+      setMyFriends(friendsWithImages);
     } catch (e) {
       console.log("Error: ", e.response?.data?.error || e.message);
-      return router.replace("/guests/Login");
     } finally {
       setIsLoading(false);
     }
-  }, [authState?.authenticated]);
+  }, []);
 
   useEffect(() => {
     console.log("useGetMyFriends" + new Date());
