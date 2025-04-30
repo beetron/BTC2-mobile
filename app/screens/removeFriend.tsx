c881a3dbb1d5d59257ed032a80ede69a6131a679
Brian <bdev@btro.jp>
2025-04-30 22:42:48 +0900

    Updated with isLoading spinner

--- app/screens/removeFriend.tsx
+++ app/screens/removeFriend.tsx
@@ -1,5 +1,5 @@
-import { View, Text } from "react-native";
-import { useState, useCallback, useEffect } from "react";
+import { View, Text, ActivityIndicator } from "react-native";
+import { useState, useEffect } from "react";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import { Image } from "expo-image";
 import { placeholderProfileImage } from "@/constants/images";
@@ -35,7 +35,7 @@ const removeFriend = () => {
     }
   };
 
-  // Handle onPress Block (planned feature)
+  // Handle onPress Block friend (planned feature)
   const handleOnPressBlock = () => {};
 
   return (
@@ -53,47 +53,55 @@ const removeFriend = () => {
             Removing a friend will remove yourself from their friend list.
           </Text>
         </View>
-        <View className="mt-2 flex-col items-start max-w-[80%]">
-          {!myFriends || myFriends.length === 0 ? (
-            <Text className="text-btc100 font-funnel-regular text-2xl">
-              You have no friends to remove
-            </Text>
-          ) : (
-            myFriends.map((friend: Friend) => (
-              <View
-                key={friend._id}
-                className="flex-row items-center justify-between w-full m-2"
-              >
-                <View className="flex-row items-center gap-4">
-                  {friend.profileImageData ? (
-                    <Image
-                      source={{ uri: friend.profileImageData }}
-                      style={{ width: 50, height: 50, borderRadius: 50 }}
-                      className="bg-btc100"
-                    />
-                  ) : (
-                    <Image
-                      source={placeholderProfileImage}
-                      style={{ width: 50, height: 50, borderRadius: 50 }}
-                      className="bg-btc100"
-                    />
-                  )}
-                  <Text className="text-btc200 font-funnel-regular text-2xl">
-                    {friend.nickname}
-                  </Text>
-                </View>
-                <View className="flex-row justify-end">
-                  <AntDesign
-                    name="closecircleo"
-                    size={28}
-                    color="red"
-                    onPress={() => handleOnPressRemove(friend.uniqueId)}
-                  />
-                </View>
-              </View>
-            ))
-          )}
-        </View>
+        {isLoading ? (
+          <View className="mt-2 items-center jusitfy-center">
+            <ActivityIndicator size="large" color="white" />
+          </View>
+        ) : (
+          <>
+            <View className="mt-2 flex-col items-start max-w-[80%]">
+              {!myFriends || myFriends.length === 0 ? (
+                <Text className="text-btc100 font-funnel-regular text-2xl">
+                  You have no friends to remove
+                </Text>
+              ) : (
+                myFriends.map((friend: Friend) => (
+                  <View
+                    key={friend._id}
+                    className="flex-row items-center justify-between w-full m-2"
+                  >
+                    <View className="flex-row items-center gap-4">
+                      {friend.profileImageData ? (
+                        <Image
+                          source={{ uri: friend.profileImageData }}
+                          style={{ width: 50, height: 50, borderRadius: 50 }}
+                          className="bg-btc100"
+                        />
+                      ) : (
+                        <Image
+                          source={placeholderProfileImage}
+                          style={{ width: 50, height: 50, borderRadius: 50 }}
+                          className="bg-btc100"
+                        />
+                      )}
+                      <Text className="text-btc200 font-funnel-regular text-2xl">
+                        {friend.nickname}
+                      </Text>
+                    </View>
+                    <View className="flex-row justify-end">
+                      <AntDesign
+                        name="closecircleo"
+                        size={28}
+                        color="red"
+                        onPress={() => handleOnPressRemove(friend.uniqueId)}
+                      />
+                    </View>
+                  </View>
+                ))
+              )}
+            </View>
+          </>
+        )}
       </View>
     </View>
   );
