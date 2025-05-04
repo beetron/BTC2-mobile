65b8f8d39024d4f0e1d4b828f9d2a9eb915cf878
Brian <bdev@btro.jp>
2025-05-04 19:44:41 +0900

    Updated primary header's text

--- app/components/HeaderPrimary.tsx
+++ app/components/HeaderPrimary.tsx
@@ -1,11 +1,16 @@
 import { View, Text } from "react-native";
 
 const HeaderPrimary = () => {
+  const appVersion = process.env.EXPO_PUBLIC_APP_VERSION;
+  console.log("App Version: ", appVersion);
   return (
-    <View className="flex-1 max-h-28 border-b-hairline border-btc100 bg-[#1f1f2e] justify-center">
-      <Text className="text-2xl text-btc100 top-5 left-5 font-funnel-medium">
-        Header
-      </Text>
+    <View className="flex-1 max-h-28 border-b-hairline border-btc100 bg-[#1f1f2e] justify-end">
+      <View className="flex-row justify-start m-4">
+        <Text className="font-funnel-medium text-3xl text-btc100">BTC2</Text>
+        <Text className="font-funnel-regular text-btc200 text-xl ml-2">
+          (v.{appVersion})
+        </Text>
+      </View>
     </View>
   );
 };
