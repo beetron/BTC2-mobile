d617606faad914a2fa20133bf6702c1232bc5dab
Brian <bdev@btro.jp>
2025-05-04 18:37:04 +0900

    Update to store the most recent message ID

--- hooks/useGetMessages.ts
+++ hooks/useGetMessages.ts
@@ -4,19 +4,11 @@ import FriendStore from "../zustand/friendStore";
 import { useAuth } from "../context/AuthContext";
 import { Alert } from "react-native";
 
-interface Friend {
-  _id: string;
-  messages: string[];
-}
-
-interface FriendStoreProps {
-  selectedFriend: Friend;
-}
-
 const useGetMessages = () => {
   const { authState } = useAuth();
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const { selectedFriend, messages, setMessages, shouldRender } = FriendStore();
+  const { selectedFriend, messages, setMessages, shouldRender, setMessageId } =
+    FriendStore();
 
   const getMessages = useCallback(async () => {
     if (selectedFriend) {
@@ -27,14 +19,22 @@ const useGetMessages = () => {
         );
         if (res.status === 200) {
           setMessages([...messages, ...res.data]);
+
+          // Set the most recent message ID for useDeleteMessages hook
+          if (res.data.length > 0) {
+            // 0 here because backend returns messages in descending order
+            const mostRecentMessageId = res.data[0];
+            setMessageId(mostRecentMessageId._id);
+          } else {
+            setMessageId(null);
+          }
         } else {
           Alert.alert("Error", "Failed to get messages");
-          // ADD REDIRECT LOGIN FOR PRODUCTION
         }
 
         setIsLoading(false);
-      } catch (e) {
-        console.log("Error: ", e.response?.data?.error || e.message);
+      } catch (error: any) {
+        console.log("Error: ", error.response?.data?.error || error.message);
         // Alert.alert("Error", e.response?.data?.error || e.message);
       } finally {
         setIsLoading(false);
