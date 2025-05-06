d33c18db84097a4871d7b927d1b8ad2347e6e9e8
Brian <bdev@btro.jp>
2025-05-06 16:45:47 +0900

    App name and logo update

--- app/components/HeaderPrimary.tsx
+++ app/components/HeaderPrimary.tsx
@@ -6,7 +6,7 @@ const HeaderPrimary = () => {
   return (
     <View className="flex-1 max-h-28 border-b-hairline border-btc100 bg-[#1f1f2e] justify-end">
       <View className="flex-row justify-start m-4">
-        <Text className="font-funnel-medium text-3xl text-btc100">BTC2</Text>
+        <Text className="font-funnel-medium text-3xl text-btc100">bTC2</Text>
         <Text className="font-funnel-regular text-btc200 text-xl ml-2">
           (v.{appVersion})
         </Text>
