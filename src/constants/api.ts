ce81aeaeb8d733897e7d2f1d847704fe29f43f39
Brian <bdev@btro.jp>
2025-11-07 21:11:32 +0900

    Update api.ts

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -3,7 +3,3 @@ export const API_URL =
   process.env.EXPO_PUBLIC_ENV === "development"
     ? process.env.EXPO_PUBLIC_API_DEV_URL
     : process.env.EXPO_PUBLIC_API_URL;
-
-// API users profile image URL
-export const API_PROFILE_IMAGE_URL =
-  process.env.EXPO_PUBLIC_API_PROFILE_IMAGE_URL;
